fcecf3dbdba494157d014ced36ffedf6a147d906
saksjain <saksjain@adobe.com>
2021-01-23 18:12:00 +0530

    testutils to inject fields

---
+++ projects/P04-eCommerce Application/starter_code/src/test/java/com/example/demo/TestUtils.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import java.lang.reflect.Field;
+
+public class TestUtils {
+
+    public static void injectObject(Object target, String fieldName, Object toInject) {
+        boolean wasPrivate = false;
+
+        try {
+            Field field = target.getClass().getDeclaredField(fieldName);
+            if(!field.canAccess(field)){
+                field.setAccessible(true);
+                wasPrivate = true;
+            }
+            field.set(target, toInject);
+            if (wasPrivate){
+                field.setAccessible(false);
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
